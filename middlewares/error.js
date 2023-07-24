6cfff4e249adace176eb372dfc6a3a905d6a3e66
Abegunde99 <preciousolanrewaju1998@gmail.com>
2023-07-24 08:08:20 +0100

    I made changes to the errorHandler

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,19 +4,16 @@ const errorHandler = (err, req, res, next)=>{
     let error = { ...err }
     error.message = err.message
 
-    //mongoose bad ObjectId
     if (err.name === "CastError") {
         const message = `resource not found with id of ${err.value}`;
         error = new ErrorResponse(message,404)
     }
 
-    //mongoose duplicate key
     if (err.code === 11000) {
         const message = "Duplicate key value entered";
         error = new ErrorResponse(message, 400)
     }
 
-    //mongoose validation error
     if (err.name === 'ValidationError') {
         const message = Object.values(err.errors).map(val => val.message)
         error = new ErrorResponse(message, 400)
